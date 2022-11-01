8314aa6e2658cb5508555037cf9870d1d12903c3
KimDaWoo <dawoo0000@naver.com>
2022-11-01 11:28:21 +0900

    PageColor

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PageColor from "./chapter_13/Sample4/PageColor";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <PageColor />
+);
